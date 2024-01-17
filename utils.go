714a4eb8ac865d917ccffc1dc971610d7eec22d9
alsiberij <alsiberij@gmail.com>
2024-01-17 17:13:03 +0300

    FEAT Ticker fixed

--- utils.go
+++ utils.go
@@ -2,15 +2,16 @@ package alslgr
 
 import "time"
 
-func TimedForwarding(interval time.Duration, doneCh <-chan struct{}) chan<- struct{} {
-	tickCh := make(chan<- struct{})
-	go tick(interval, tickCh, doneCh)
-	return tickCh
+func TimedForwarding(interval time.Duration, doneCh <-chan struct{}) <-chan struct{} {
+	sigCh := make(chan struct{})
+	go tick(interval, sigCh, doneCh)
+	return sigCh
 }
 
 func tick(interval time.Duration, sigCh chan<- struct{}, doneCh <-chan struct{}) {
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
+	defer close(sigCh)
 
 	for {
 		select {
