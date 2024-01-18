1f84b94fb7a490e4c2f7774006812f41ea7bb0b3
alsiberij <alsiberij@gmail.com>
2024-01-19 00:21:16 +0300

    REFACTOR Major refactor

---
+++ ticker.go
@@ -0,0 +1,24 @@
+package alslgr
+
+import "time"
+
+func NewTicker(interval time.Duration, doneCh <-chan struct{}) <-chan struct{} {
+	sigCh := make(chan struct{})
+	go tick(interval, sigCh, doneCh)
+	return sigCh
+}
+
+func tick(interval time.Duration, sigCh chan<- struct{}, doneCh <-chan struct{}) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	defer close(sigCh)
+
+	for {
+		select {
+		case <-ticker.C:
+			sigCh <- struct{}{}
+		case <-doneCh:
+			return
+		}
+	}
+}
